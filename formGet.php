228dfb2edeb5bdf613f4c1868cda329883a6624b
iqbalali <iqbalalisaidil@outlook.com>
2021-01-23 10:58:44 +0700

    Form Get in php

---
+++ formGet.php
@@ -0,0 +1,19 @@
+//Get -> adalah variabel yang dikirimkan melalui URL
+dan ditampilkan pada URL//
+<html>
+    <head>
+        <title>Form Get</title>
+    </head>
+
+    <body>
+
+        <form action="tangkapGet.php" method="Get">
+            Nama : 
+            <input type="text" name="nama" />
+            <br/>
+            <input type="submit" value="kirim" />
+
+        </form>
+    </body>
+
+</html>  
