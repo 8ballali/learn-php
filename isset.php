860f2d047463292e22450b46bbec47bc9ae8c24a
iqbalali <iqbalalisaidil@outlook.com>
2021-01-23 19:10:56 +0700

    isset in php

---
+++ isset.php
@@ -0,0 +1,13 @@
+/* ISSET digunakan untuk mengecek apakah variable tersebut ada atau tidak
+    Aapabila ada maka akan bernilai True dan apabila tidak ada maka akan bernilai false */
+<?php
+    
+        $buah = "Semangka";
+        $kendaraan = "Sepeda";
+
+        echo "Hasil Variable buah :" . isset($buah);
+        echo "<br/>";
+        echo "Hasil Variable kendaraan :" . isset($kendaraan);
+        echo "<br/>";
+        echo "Hasil variabel minuman :" . isset($minuman);
+?>
